6e351e0715dc32353995212dd8674f2e83abd41a
attitudeshuai <848001189@qq.com>
2026-06-18 03:28:02 +0800

    upload frontend-admin/src/router/index.js

---
+++ frontend-admin/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/HomeView.vue'),
+    meta: { title: '首页' }
+  },
+  {
+    path: '/timeline',
+    name: 'Timeline',
+    component: () => import('@/views/TimelineView.vue'),
+    meta: { title: '生平时间线' }
+  },
+  {
+    path: '/persons',
+    name: 'Persons',
+    component: () => import('@/views/PersonsView.vue'),
+    meta: { title: '人物列表' }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/views/AboutView.vue'),
+    meta: { title: '关于项目' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title || '苏轼社交网络'} - 苏轼社交网络可视化`
+  next()
+})
+
+export default router
